f835cac365a9f4230db94a57698710838da26185
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-10-20 12:21:25 +0300

    Добавьте файлы проекта.

---
+++ PRV_Mandelbrot_set/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRV_Mandelbrot_set
+{
+    public partial class Form1 : Form
+    {
+        Mandelbrot mandelbrot;
+        public Form1()
+        {
+            InitializeComponent();
+
+            mandelbrot = new Mandelbrot(panel1.Width, panel1.Height);
+
+            Graphics gr = panel1.CreateGraphics();
+
+            mandelbrot.toPicture();
+            //Image image = new Image(mandelbrot.picture);
+            //gr.Dra
+            
+            //gr.DrawRectangle(pen, 10, 10, 100, 100);
+            //gr.DrawImage(mandelbrot.picture, 0, 0);
+            //panel1.Refresh();
+            //Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 10);
+            //e.Graphics.DrawRectangle(pen, 10, 10, 100, 100);
+            e.Graphics.DrawImage(mandelbrot.picture, 0, 0);
+        }
+    }
+}
---
+++ PRV_Mandelbrot_set/Mandelbrot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRV_Mandelbrot_set
+{
+    class Mandelbrot
+    {
+        Complex c;
+        Complex z = 0;
+        double x, y;
+        int width, height;
+        int xCenter, yCenter;
+        public Bitmap picture;// = new Bitmap();
+        int maxIterations = 300;
+        int infinityBorder = 4;
+
+        public Mandelbrot(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            picture = new Bitmap(width, height);
+
+            xCenter = width / 3 * 2;
+            yCenter = height / 2;
+            toPicture();
+        }
+        
+        public Complex InMandelbrotSet(Complex c)
+        {
+            z = Complex.Zero;
+
+            for (int i = 0; i < maxIterations; i++)
+            {
+                z = Complex.Pow(z, 2) + c;
+
+                if (z.Magnitude > infinityBorder) break;
+            }
+
+            return z;
+        }
+
+        public void toPicture()
+        {
+            int startX = 0 - xCenter;
+            int startY = 0 - yCenter;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    double relX = ((double)i + (double)startX) / (double)width ;
+                    double relY = ((double)j + (double)startY) / (double)height ;
+
+                    Complex c = InMandelbrotSet(new Complex(relX, relY));
+
+                    if (Math.Abs(c.Magnitude) > 2) picture.SetPixel(i, j, Color.White);
+                    else picture.SetPixel(i, j, Color.Black);
+                }
+            }
+        }
+
+     
+
+    }
+}
